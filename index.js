999670e32343291a19b965d13a2f4083d53c67c0
Dharamveer <dharammveer945734@outlook.com>
2023-06-15 23:30:00 +0530

    Final Commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const {productRouter} = require("./routes/product.route")
 app.use("/users",userRouter)
 app.use("/products", productRouter)
 
-const abc = 123
+const abc = 12
 
 app.listen(4500,async()=>{
     try {
