0cd9783fed013bdd717f3ffca06fa1fc5818eb25
Dharamveer <dharammveer945734@outlook.com>
2023-06-20 23:52:22 +0530

    Final commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {connection} = require("./config/db")
 const {userRouter} = require("./routes/user.route")
 const {productRouter} = require("./routes/product.route")
 
-const abc = 12
+const abc = 1
 app.use("/users",userRouter)
 app.use("/products", productRouter)
 
